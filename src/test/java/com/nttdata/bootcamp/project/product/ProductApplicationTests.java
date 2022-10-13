cea3fe0581ec2e60110c1491f3e1fa4710dbfe4e
marc-dev2 <115658108+marc-dev2@users.noreply.github.com>
2022-10-13 14:37:09 -0500

    initial commit

---
+++ src/test/java/com/nttdata/bootcamp/project/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.project.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
